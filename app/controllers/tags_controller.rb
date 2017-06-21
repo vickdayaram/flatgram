f0362208d8c07ba91c7f01c2590ca72a398b3a13
github username <github email address>
2017-06-21 09:36:43 -0400

    in the middle of setting up followers relationship, right before routes setup

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,8 @@
 class TagsController < ApplicationController
+
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,34 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   has_secure_password
 
-  has_many :comments
-  has_many :pictures
+  has_many :comments, dependent: :destroy #if user account is deleted, all their
+  has_many :pictures, dependent: :destroy # pictures and comments will be as well.
+
+  has_many :active_relationships, class_name:"Relationship", foreign_key:"follwer_id", dependent: :destroy
+  has_many :passive_relationships, class_name:"Relationship", foreign_key:"followed_id", dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  #helper methods for following feature
+
+  #follow another user
+  def follow(other)
+    active_relationships.create(followed_id: other.id)
+  end
+
+  #unfollow a user
+  def unfollow(other)
+    active_relationships.find_by(followed_id: other.id).destroy
+  end
+
+  #is following?
+  def following?(other)
+    following.include?(other)
+  end
 
 
   #may be working be re test as you add more data.
